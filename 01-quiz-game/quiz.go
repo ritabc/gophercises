dabae2e32ee41e30618ef5a0a6182ac8f7d32d51
ritabc <ritabc@gmail.com>
2019-10-28 17:16:36 -0400

    Make changes and complete after watching video for 01-Quiz

--- 01-quiz-game/quiz.go
+++ 01-quiz-game/quiz.go
@@ -5,9 +5,9 @@ import (
 	"encoding/csv"
 	"flag"
 	"fmt"
-	"io"
 	"os"
 	"strings"
+	"time"
 )
 
 type question struct {
@@ -16,39 +16,58 @@ type question struct {
 }
 
 func main() {
-	filenamePtr := flag.String("filename", "questions.csv", "File name for quiz questions")
+	filename := flag.String("csv", "questions.csv", "File name for quiz questions")
+	timeLimitPtr := flag.Int("limit", 3, "Time limit for entire quiz, in seconds")
 	flag.Parse()
-	fmt.Println("Welcome to the CLI")
-	fmt.Println("Filename Entered: ", *filenamePtr)
-	csvFile, err := os.Open(*filenamePtr)
+	fmt.Println("Welcome to the Quiz")
+	csvFile, err := os.Open(*filename)
 	if err != nil {
-		fmt.Errorf("Error opening CSV file: ", err.Error)
+		fmt.Printf("Error opening CSV file: %s\n", *filename)
+		os.Exit(1)
 	}
-	reader := csv.NewReader(csvFile)
+	csvReader := csv.NewReader(csvFile)
 	var questions []question
-	for {
-		line, err := reader.Read()
-		if err == io.EOF {
-			break
-		} else if err != nil {
-			fmt.Errorf("Error reading question: ", err.Error)
-		}
+	lines, err := csvReader.ReadAll()
+	if err != nil {
+		fmt.Println("Error parsing the provided CSV file.")
+		os.Exit(1)
+	}
+	for _, line := range lines {
 		questions = append(questions, question{
 			text:   line[0],
 			answer: line[1],
 		})
 	}
 	var score int
+	timeUpChan := make(chan struct{})
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Press enter to start quiz clock")
+	_, err = reader.ReadString('\n')
+	if err == nil {
+		go func() {
+			limit := time.Duration(*timeLimitPtr) * time.Second
+			fmt.Println(limit)
+			time.Sleep(limit)
+			timeUpChan <- struct{}{}
+		}()
+	}
 	for _, question := range questions {
 		fmt.Println("What is: ", question.text)
-		ansReader := bufio.NewReader(os.Stdin)
-		userAnswer, err := ansReader.ReadString('\n')
-		if err != nil {
-			fmt.Errorf("Error reading answer: ", err.Error)
-		}
-		userAnswer = strings.TrimSuffix(userAnswer, "\n")
-		if question.answer == userAnswer {
-			score++
+		answerCh := make(chan string)
+		go func() {
+			userAnswer, _ := reader.ReadString('\n')
+			userAnswer = strings.TrimSuffix(userAnswer, "\n")
+			answerCh <- userAnswer
+		}()
+		select {
+		case <-timeUpChan:
+			fmt.Println("\nTime's Up!")
+			fmt.Printf("You correctly answered %d out of %d\n", score, len(questions))
+			return
+		case userAnswer := <-answerCh:
+			if question.answer == userAnswer {
+				score++
+			}
 		}
 	}
 	fmt.Printf("You correctly answered %d out of %d\n", score, len(questions))
