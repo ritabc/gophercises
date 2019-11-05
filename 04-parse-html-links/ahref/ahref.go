e6b4166ff1e0fffb75b4ae58cb2ac9b0b2eb3876
ritabc <ritabc@gmail.com>
2019-11-04 19:40:32 -0500

    Set up test structure - first pass

--- 04-parse-html-links/ahref/ahref.go
+++ 04-parse-html-links/ahref/ahref.go
@@ -1,1 +1,13 @@
+// Package ahref assists in parsing an HTML file and extracting links
 package ahref
+
+// Ahref is an <a> tag composed of link and text displayed
+type Ahref struct {
+	Href string
+	Text string
+}
+
+// ParseAhref should ultimately return []Ahref
+func ParseAhref(html string) string {
+	return html
+}
--- 04-parse-html-links/ahref/ahref_test.go
+++ 04-parse-html-links/ahref/ahref_test.go
@@ -1,18 +1,50 @@
 package ahref
 
 import (
+	"fmt"
+	"io/ioutil"
+	"os"
 	"testing"
 )
 
 var testCases = []struct {
 	exFile string
-	output Href
-}{}
+	output Ahref
+}{
+	{
+		exFile: "./examples/one.html",
+		output: Ahref{},
+	},
+	{
+		exFile: "./examples/two.html",
+		output: Ahref{},
+	},
+	{
+		exFile: "./examples/three.html",
+		output: Ahref{},
+	},
+	{
+		exFile: "./examples/four.html",
+		output: Ahref{},
+	},
+}
 
-func TestHref(t *testing.T) {
+func TestParseAhref(t *testing.T) {
 	for _, test := range testCases {
-		// if ok := Valid(test.input); ok != test.ok {
-		// 	t.Fatalf("Valid(%s): %s\n\t Expected: %t\n\t Got: %t", test.input, test.description, test.ok, ok)
-		// }
+		file, err := os.Open(test.exFile)
+		if err != nil {
+			t.Errorf("Could not open file: %s\n", test.exFile)
+		}
+		defer file.Close()
+
+		contents, err := ioutil.ReadAll(file)
+		if err != nil {
+			t.Errorf("Could not read file: %s\n", test.exFile)
+		}
+
+		fmt.Printf("%s\n\n\n\n\n\n", string(contents))
+		if string(contents) != ParseAhref(string(contents)) {
+			t.Errorf("Not a match")
+		}
 	}
 }
